f9b0db1c88a174b1efcc86f07755de6e87d58c78
amog787 <amoghmaiya@gmail.com>
2019-01-15 13:27:37 +0530

    Bootleggers Bringup

--- bootleg.mk
+++ bootleg.mk
@@ -18,16 +18,22 @@
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := lineage_kenzo
+PRODUCT_NAME := bootleg_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
+
+# Bootleg Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="amog787"
+
+TARGET_PICK_BOOTANIMATION := "1,2,4"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo bootleg_kenzo-user
+add_lunch_combo bootleg_kenzo-userdebug
+add_lunch_combo bootleg_kenzo-eng
